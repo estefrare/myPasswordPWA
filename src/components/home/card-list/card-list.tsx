27cd40ceec921bfd5835aac0778fbe57a9bd2e0d
Esteban Frare <esteban.frare@radiumrocket.com>
2020-05-04 10:31:09 -0300

    PSW: add search input

--- src/components/home/card-list/card-list.tsx
+++ src/components/home/card-list/card-list.tsx
@@ -1,19 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Card from './card'
 import { ReduxProps } from '.'
 import styles from './card-list.module.css'
 
 export const CardList = (props: ReduxProps) => {
 
-  const { getServices } = props
+  const { getServices, serviceList } = props
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     getServices()
   }, [getServices])
 
+  const filterList = useMemo(() => {
+    return serviceList.filter((service) =>
+      service.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase()),
+    );
+  }, [filter, serviceList]);
+
   return (
     <div className={styles.container}>
-      {props.serviceList.map((service) => <Card key={service.name} service={service} />)}
+      <div>
+        <input
+          placeholder="Search"
+          className={styles.input}
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)} 
+          />
+        <i className="material-icons">search</i>
+      </div>
+      <div className={styles.list}>
+        {filterList.map((service) => <Card key={service.name} service={service} />)}
+      </div>
     </div>
   )
 }
